c7d042b5dbae344f5f61d5556c30609e141fd849
Srikalyan Megati <kalmegati@gmail.com>
2019-09-20 19:43:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/(?\d{3}[ -)]?\d{3}[ -]?\d{4}/)
+  if phone.scan(/\A(?\d{3}[ -)]?\d{3}[ -]?\d{4}\z/)
 end
