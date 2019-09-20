e8a4a207ae296b81f190377100da9ebee4414a7d
Srikalyan Megati <kalmegati@gmail.com>
2019-09-20 19:21:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word == /\b[aeiouAEIOU]\w+/
-    true
-  else
-    false
-  end
+  word.match(/\b[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
